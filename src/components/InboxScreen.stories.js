1a79ecea7142e5a115e32f793939fd1f80b3ce10
torouso <torouso@naver.com>
2022-08-26 10:04:43 +0900

    change task background to red

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import InboxScreen from './InboxScreen';
-
 import store from '../lib/store';
 import { rest } from 'msw';
 import { MockedState } from './TaskList.stories';
